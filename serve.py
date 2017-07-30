c09377d09d7f17aaedc8ca601d277d7d7ae369c7
Oscar Hong <oscar.hong2015@gmail.com>
2017-07-30 02:42:13 -0700

    image upload and decoding works

--- serve.py
+++ serve.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template, request
 from flask import jsonify
 # from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import uuid
+import base64
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -25,15 +27,28 @@ def home():
         "status": "good"
     })
 
-
-@app.route('/img')
+@app.route('/img', methods = ['POST'])
 def img():
+    content = request.json
+    encoded_img = content["encoded_img"] # the base64 of the img
+    decoded_img = base64.b64decode(encoded_img)
+    img_id = str(uuid.uuid4())
+    # save the image
+    fout1 = open("./images/line/" + img_id + ".png", 'wb')
+    fout1.write(decoded_img)
+    fout1.close()
+    # save the ref
+    fout1 = open("./images/ref/" + img_id + ".png", 'wb')
+    fout1.write(decoded_img)
+    fout1.close()
+
     return jsonify({
-        "todo": "todo"
+        "id": img_id,
     })
 
-@app.route('/ref')
+@app.route('/ref', methods = ['POST'])
 def ref():
+    # STUB
     return jsonify({
         "todo": "todo"
     })
