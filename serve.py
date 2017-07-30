60c9f398f19417a7604d0656430444a8814920cd
Keon Kim <keon@uber.com>
2017-07-30 01:24:13 -0700

    Add flask boilerplate

--- serve.py
+++ serve.py
@@ -1,1 +1,51 @@
-# TODO: 
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+from flask import Flask, render_template, request
+from flask import jsonify
+# from flask.ext.sqlalchemy import SQLAlchemy
+import os
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+
+#----------------------------------------------------------------------------#
+# Controllers.
+#----------------------------------------------------------------------------#
+
+
+@app.route('/')
+def home():
+    return jsonify({
+        name: "pretty-whale",
+        status: "good"
+    })
+
+
+@app.route('/img')
+def img():
+    return jsonify({
+        todo: "todo"
+    })
+
+@app.route('/ref')
+def ref():
+    return jsonify({
+        todo: "todo"
+    })
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({
+        "status": "404 Not Found"
+    })
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
